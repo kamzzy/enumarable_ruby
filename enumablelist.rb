a2efd15055194afbe162a598c2badb95322e4dae
Regiss Mukubiza <regiss.steven@gmail.com>
2022-07-22 14:28:28 +0200

    conditions

--- enumablelist.rb
+++ enumablelist.rb
@@ -11,3 +11,15 @@ class MyList
     @list.each(&block)
   end
 end
+
+list = MyList.new(1, 2, 3, 4)
+
+p(list.all? { |item| item < 5 })
+
+p(list.all? { |item| item > 5 })
+
+p(list.any? { |item| item == 2 })
+
+p(list.any? { |item| item == 5 })
+
+p(list.filter(&:even?))
