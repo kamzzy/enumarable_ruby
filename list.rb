4cde17f64e60db2947e981f2b2c7220f0510965f
Precious Akams <preciousakams@yahoo.com>
2022-07-22 13:12:23 +0100

    enumerable

---
+++ list.rb
@@ -0,0 +1,12 @@
+Class MyList
+include MyEnumerable
+
+  # @list = []
+  # def initialize(list)
+  #   @list = list
+  # end
+
+  # def each
+  #   @list.each { |a| a.each { |x| yield x } }
+  # end
+end
